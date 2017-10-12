f272b37f5e46ea5b20a79e613fb0ebe472211cbf
Jauhar Arifin <jauhararifin10@gmail.com>
2017-10-12 19:41:11 +0700

    add karatsuba multiply algorithm

--- template/polynomial.cpp
+++ template/polynomial.cpp
@@ -36,7 +36,7 @@ public:
 	friend polynomial<S> operator-(polynomial<S> a);
 
 	template<typename S>
-	friend polynomial<S> operator-(polynomial<S>& a, polynomial<S>& b);
+	friend polynomial<S> operator-(polynomial<S> a, polynomial<S> b);
 };
 
 template<typename T>
@@ -81,18 +81,54 @@ polynomial<S> operator-(polynomial<S> a) {
 };
 
 template<typename S>
-polynomial<S> operator-(polynomial<S>& a, polynomial<S>& b) {
+polynomial<S> operator-(polynomial<S> a, polynomial<S> b) {
 	return a + (-b);
 };
 
+template<typename S>
+polynomial<S> karatsuba_multiply(polynomial<S> a, polynomial<S> b) {
+	if (a.degree() <= 1 || b.degree() <= 1)
+		return a*b;
+	polynomial<S> x0,x1,y0,y1;
+	int m = max(a.degree(), b.degree())/2;
+	for (int i = 0; i <= a.degree(); i++)
+		if (i < m)
+			x0[i] = a[i];
+		else
+			x1[i-m] = a[i];
+	for (int i = 0; i <= b.degree(); i++)
+		if (i < m)
+			y0[i] = b[i];
+		else
+			y1[i-m] = b[i];
+	polynomial<S> z2 = karatsuba_multiply(x1,y1);
+	polynomial<S> z0 = karatsuba_multiply(x0,y0);
+	polynomial<S> z1 = karatsuba_multiply(x1 + x0,y1 + y0) - z2 - z0;
+
+	for (int i = 0; i <= z1.degree(); i++)
+		z0[i+m] += z1[i];
+	for (int i = 0; i <= z2.degree(); i++)
+		z0[i+2*m] += z2[i];
+
+	return z0;
+}
+
 int main() {
-	polynomial<int> p;
-	p[5] = 5;
-	p[3] = 2;
-	polynomial<int> q;
-	q[1] = 2;
-	q[3] = 5;
-	q[7] = 9;
-	cout<<p<<endl<<q<<endl<<(p+q)<<endl<<(-q)<<endl<<(p-q)<<endl<<p*q<<endl;
+	ios::sync_with_stdio(0);
+	int t,n;
+	cin>>t;
+	while (t--) {
+		cin>>n;
+		polynomial<int> a,b;
+		for (int i = 0; i < n+1; i++)
+			cin>>a[n-i];
+		for (int i = 0; i < n+1; i++)
+			cin>>b[n-i];
+		polynomial<int> c = karatsuba_multiply(a,b);
+
+		for (int i = c.degree(); i > 0; i--)
+			cout<<c[i]<<" ";
+		cout<<c[0]<<endl;
+	}
 	return 0;
 }
