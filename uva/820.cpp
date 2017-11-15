72735359cd63b7037d59d0f9625707bacf80d36f
Jauhar Arifin <jauhararifin10@gmail.com>
2017-11-15 18:49:55 +0700

    add solution to uva820

---
+++ uva/820.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename T>
+T dinic_flow(vector<pair<int,T> >* adjlist, int* level, T** flow, T** capacity, int s, int t, T aug) {
+	if (aug == 0 || s == t) return aug;
+	T total_flow = 0;
+	for (int i = 0; aug != 0 && i < (int) adjlist[s].size(); i++) {
+		pair<int,T> edge = adjlist[s][i];
+		if (level[s] + 1 == level[edge.first]) {
+			T new_aug = capacity[s][edge.first] - flow[s][edge.first];
+			T new_flow = dinic_flow(adjlist, level, flow, capacity, edge.first, t, aug < 0 ? new_aug : min(aug, new_aug));
+			total_flow += new_flow;
+			flow[s][edge.first] += new_flow;
+			flow[edge.first][s] -= new_flow;
+			aug -= new_flow;
+		}
+	}
+	return total_flow;
+}
+template<typename T>
+T dinic(int n, vector<pair<int,T> >* adjlist, int s, int t) {
+	T maxflow = 0;
+
+	T** flow = new T*[n];
+	T** capacity = new T*[n];
+	vector<int> *adj = new vector<int>[n];
+	for (int i = 0; i < n; i++) {
+		flow[i] = new T[n];
+		capacity[i] = new T[n];
+		for (int j = 0; j < n; j++)
+			flow[i][j] = 0, capacity[i][j] = 0;
+		for (int j = 0; j < (int) adjlist[i].size(); j++) {
+			if (capacity[i][adjlist[i][j].first] == 0)
+				adj[i].push_back(adjlist[i][j].first);
+			capacity[i][adjlist[i][j].first] += adjlist[i][j].second;
+		}
+	}
+	int* level = new int[n];
+
+	while (1) {
+		for (int i = 0; i < n; i++) level[i] = -1;
+		level[s] = 0;
+		queue<int> que; que.push(s);
+		while (!que.empty()) {
+			int t = que.front(); que.pop();
+			for (int i = 0; i < (int) adj[t].size(); i++) {
+				int next = adj[t][i];
+				if (level[next] < 0 && flow[t][next] < capacity[t][next]) {
+					level[next] = level[t] + 1;
+					que.push(next);
+				}
+			}
+		}
+		if (level[t] < 0)
+			break;
+		T new_flow;
+		while ((new_flow = dinic_flow(adjlist, level, flow, capacity, s, t, (T) -1)) > 0)
+			maxflow += new_flow;
+	}
+	
+	for (int i = 0; i < n; i++)
+		delete [] flow[i], delete [] capacity[i];
+	delete [] adj;
+	delete [] flow;
+	delete [] capacity;
+	delete [] level;
+
+	return maxflow;
+}
+
+int n,s,t,c;
+vector<pair<int,long long> > adj[5000];
+
+int main() {
+	for (int tc = 1;; tc++) {
+		scanf("%d", &n);
+		if (n == 0) break;
+		scanf("%d%d%d", &s, &t, &c);
+
+		for (int i = 0; i < n; i++) adj[i].clear();
+
+		while (c--) {
+			int a,b; long long c; scanf("%d%d%lld", &a, &b, &c);
+			adj[a-1].push_back(make_pair(b-1,c));
+			adj[b-1].push_back(make_pair(a-1,c));
+		}
+		printf("Network %d\nThe bandwidth is %lld.\n\n", tc, dinic(n, adj, s - 1, t - 1));
+	}
+	return 0;
+}
