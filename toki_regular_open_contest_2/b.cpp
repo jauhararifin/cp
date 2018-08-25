c84c0882ceb2bbc77ec95e7c72f5283d24af707d
Jauhar Arifin <jauhararifin10@gmail.com>
2018-08-25 21:49:36 +0700

    add troc#2 solutions

---
+++ toki_regular_open_contest_2/b.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int main() {
+  scanf("%d", &n);
+  printf("%d\n1", n - 1);
+  for (int i = 0; i < n - 3; i++)
+    printf(" 1");
+  printf(" 2\n");
+  return 0;
+}
